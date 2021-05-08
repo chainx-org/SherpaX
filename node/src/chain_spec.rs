d06f4385d9968c233d82d11e248ab69d8c47e22e
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-05-08 10:58:03 +0800

    Init pcx and xbtc in assets

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -15,18 +15,24 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
-use cumulus_primitives_core::ParaId;
-use dev_parachain_primitives::{AccountId, Signature};
-use dev_parachain_runtime::{
-    constants::currency::DOTS, BalancesConfig, GenesisConfig, ParachainInfoConfig, SudoConfig,
-    SystemConfig,
-};
 use hex_literal::hex;
+use serde::{Deserialize, Serialize};
+
 use sc_chain_spec::{ChainSpecExtension, ChainSpecGroup};
 use sc_service::{ChainType, Properties};
-use serde::{Deserialize, Serialize};
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
+
+use cumulus_primitives_core::ParaId;
+
+use xpallet_assets::AssetRestrictions;
+use xpallet_assets_registrar::{AssetInfo, Chain};
+
+use dev_parachain_primitives::{AccountId, Signature};
+use dev_parachain_runtime::{
+    constants::currency::DOTS, BalancesConfig, GenesisConfig, ParachainInfoConfig, Runtime,
+    SudoConfig, SystemConfig, XAssetsConfig, XAssetsRegistrarConfig,
+};
 
 /// Specialized `ChainSpec` for the normal parachain runtime.
 pub type ChainSpec = sc_service::GenericChainSpec<dev_parachain_runtime::GenesisConfig, Extensions>;
@@ -95,6 +101,43 @@ pub fn get_chain_spec(id: ParaId) -> Result<ChainSpec, String> {
     ))
 }
 
+type AssetId = u32;
+
+const PCX: AssetId = 0;
+const PCX_DECIMALS: u8 = 8;
+fn pcx_restrictions() -> AssetRestrictions {
+    AssetRestrictions::DEPOSIT
+        | AssetRestrictions::WITHDRAW
+        | AssetRestrictions::DESTROY_WITHDRAWAL
+        | AssetRestrictions::DESTROY_USABLE
+}
+
+const X_BTC: AssetId = 1;
+const BTC_DECIMALS: u8 = 8;
+const X_BTC_ASSETRESTRICTIONS: AssetRestrictions = AssetRestrictions::DESTROY_USABLE;
+
+fn pcx_asset_info() -> AssetInfo {
+    AssetInfo::new::<Runtime>(
+        b"PCX".to_vec(),
+        b"Polkadot ChainX".to_vec(),
+        Chain::ChainX,
+        PCX_DECIMALS,
+        b"ChainX's crypto currency in Polkadot ecology".to_vec(),
+    )
+    .unwrap()
+}
+
+fn xbtc_asset_info() -> AssetInfo {
+    AssetInfo::new::<Runtime>(
+        b"XBTC".to_vec(),
+        b"ChainX Bitcoin".to_vec(),
+        Chain::Bitcoin,
+        BTC_DECIMALS,
+        b"ChainX's Cross-chain Bitcoin".to_vec(),
+    )
+    .unwrap()
+}
+
 fn testnet_genesis(
     root_key: AccountId,
     endowed_accounts: Vec<AccountId>,
@@ -122,8 +165,15 @@ fn testnet_genesis(
         },
         pallet_sudo: SudoConfig { key: root_key },
         parachain_info: ParachainInfoConfig { parachain_id: id },
-        // FIXME: initialize them properly
-        xpallet_assets_registrar: Default::default(),
-        xpallet_assets: Default::default(),
+        xpallet_assets_registrar: XAssetsRegistrarConfig {
+            assets: vec![
+                (PCX, pcx_asset_info(), true, false),
+                (X_BTC, xbtc_asset_info(), true, true),
+            ],
+        },
+        xpallet_assets: XAssetsConfig {
+            assets_restrictions: vec![(PCX, pcx_restrictions()), (X_BTC, X_BTC_ASSETRESTRICTIONS)],
+            endowed: Default::default(), // FIXME: maybe issue some asset balances?
+        },
     }
 }
