559c7815ee7c1eef0be9193759ec5be5814726d1
daiwei <daiwei@daiweideMacBook-Air.local>
2021-05-12 07:19:19 +0800

    Impl swap rpc

--- pallets/swap/src/rpc.rs
+++ pallets/swap/src/rpc.rs
@@ -1,11 +1,13 @@
 
 use super::*;
 impl<T: Config> Pallet<T> {
-    pub fn supply_out_amount(supply: u128, path: Vec<AssetId>) -> u128 {
-        todo!("Self::get_amount_out_by_path(supply, &path).map_or(0u128, |amounts| *amounts.last().unwrap_or(0u128))")
+    pub fn supply_out_amount(supply: BalanceOf<T>, path: Vec<AssetId>) -> BalanceOf<T> {
+        Self::get_amount_out_by_path(supply, &path).
+            map_or(Default::default(), |amounts| *amounts.last().unwrap_or(&Default::default()))
     }
 
-    pub fn desired_in_amount(desired_amount: u128, path: Vec<AssetId>) -> u128 {
-        todo!("Self::get_amount_in_by_path(desired_amount, &path).map_or(0u128, |amounts| *amounts.first().unwrap_or(0u128))")
+    pub fn desired_in_amount(desired_amount: BalanceOf<T>, path: Vec<AssetId>) -> BalanceOf<T> {
+        Self::get_amount_in_by_path(desired_amount, &path)
+            .map_or(Default::default(), |amounts| *amounts.first().unwrap_or(&Default::default()))
     }
 }
