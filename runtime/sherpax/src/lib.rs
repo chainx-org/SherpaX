7be1265b553bc8a7f17be06527bc5f88b2ffd020
icodezjb <icodezjb@users.noreply.github.com>
2021-12-28 11:21:50 +0800

    Add for test

--- runtime/sherpax/src/lib.rs
+++ runtime/sherpax/src/lib.rs
@@ -365,7 +365,7 @@ impl pallet_utility::Config for Runtime {
 }
 
 parameter_types! {
-    pub const CouncilMotionDuration: BlockNumber = 7 * DAYS;
+    pub const CouncilMotionDuration: BlockNumber = 7 * MINUTES;
     pub const CouncilMaxProposals: u32 = 100;
     pub const CouncilMaxMembers: u32 = 100;
 }
@@ -382,7 +382,7 @@ impl pallet_collective::Config<CouncilCollective> for Runtime {
 }
 
 parameter_types! {
-    pub const TechnicalMotionDuration: BlockNumber = 5 * DAYS;
+    pub const TechnicalMotionDuration: BlockNumber = 5 * MINUTES;
     pub const TechnicalMaxProposals: u32 = 100;
     pub const TechnicalMaxMembers: u32 = 100;
 }
@@ -424,9 +424,9 @@ parameter_types! {
     // additional data per vote is 32 bytes (account id).
     pub const VotingBondFactor: Balance = deposit(0, 32);
     pub const VotingBond: Balance = UNITS / 100;
-    pub const TermDuration: BlockNumber = DAYS;
-    pub const DesiredMembers: u32 = 11;
-    pub const DesiredRunnersUp: u32 = 7;
+    pub const TermDuration: BlockNumber = 2 * MINUTES;
+    pub const DesiredMembers: u32 = 3;
+    pub const DesiredRunnersUp: u32 = 3;
     pub const ElectionsPhragmenPalletId: LockIdentifier = *b"ksx/phre";
 }
 // Make sure that there are no more than `MaxMembers` members elected via elections-phragmen.
@@ -491,14 +491,14 @@ impl pallet_identity::Config for Runtime {
 }
 
 parameter_types! {
-    pub const LaunchPeriod: BlockNumber = 7 * DAYS;
-    pub const VotingPeriod: BlockNumber = 7 * DAYS;
-    pub const FastTrackVotingPeriod: BlockNumber = 3 * HOURS;
+    pub const LaunchPeriod: BlockNumber = 7 * MINUTES;
+    pub const VotingPeriod: BlockNumber = 7 * MINUTES;
+    pub const FastTrackVotingPeriod: BlockNumber = 3 * MINUTES;
     pub const InstantAllowed: bool = true;
     // 10 PCX
     pub const MinimumDeposit: Balance = 1000 * UNITS;
-    pub const EnactmentPeriod: BlockNumber = 8 * DAYS;
-    pub const CooloffPeriod: BlockNumber = 7 * DAYS;
+    pub const EnactmentPeriod: BlockNumber = 8 * MINUTES;
+    pub const CooloffPeriod: BlockNumber = 7 * MINUTES;
     // One cent: $10,000 / MB
     pub const PreimageByteDeposit: Balance = CENTS;
     pub const MaxVotes: u32 = 100;
@@ -560,16 +560,16 @@ parameter_types! {
     pub const ProposalBond: Permill = Permill::from_percent(5);
     // 10 PCX
     pub const ProposalBondMinimum: Balance = 1000 * UNITS;
-    pub const SpendPeriod: BlockNumber = 6 * DAYS;
+    pub const SpendPeriod: BlockNumber = 6 * MINUTES;
     pub const NoBurn: Permill = Permill::from_percent(0);
-    pub const TipCountdown: BlockNumber = 1 * DAYS;
+    pub const TipCountdown: BlockNumber = MINUTES;
     pub const TipFindersFee: Percent = Percent::from_percent(20);
-    pub const TipReportDepositBase: Balance = 1 * UNITS;
-    pub const DataDepositPerByte: Balance = 1 * CENTS;
-    pub const BountyDepositBase: Balance = 1 * UNITS;
-    pub const BountyDepositPayoutDelay: BlockNumber = 4 * DAYS;
+    pub const TipReportDepositBase: Balance = UNITS;
+    pub const DataDepositPerByte: Balance = CENTS;
+    pub const BountyDepositBase: Balance = UNITS;
+    pub const BountyDepositPayoutDelay: BlockNumber = 4 * MINUTES;
     pub const TreasuryPalletId: PalletId = PalletId(*b"pcx/trsy");
-    pub const BountyUpdatePeriod: BlockNumber = 90 * DAYS;
+    pub const BountyUpdatePeriod: BlockNumber = 90 * MINUTES;
     pub const MaximumReasonLength: u32 = 16384;
     pub const BountyCuratorDeposit: Permill = Permill::from_percent(50);
     pub const BountyValueMinimum: Balance = 10 * UNITS;
