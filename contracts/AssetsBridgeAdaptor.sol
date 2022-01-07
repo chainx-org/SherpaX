96bcd8127934d322442739e250bc88c4f7928d1f
icodezjb <icodezjb@users.noreply.github.com>
2022-01-07 15:33:59 +0800

    Fix AssetsBridgeRequire

--- contracts/AssetsBridgeAdaptor.sol
+++ contracts/AssetsBridgeAdaptor.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.1/contracts/utils/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.1/contracts/access/Ownable.sol";
 
 /**
  * @dev Interface of the SherpaX AssetsBridge
@@ -23,11 +24,21 @@ interface IAssetsBridge {
     function burn_from(address account, uint256 amount) external returns (bool);
 }
 
-abstract contract AssetsBridgeModifier is Context {
+abstract contract AssetsBridgeAdmin is Context {
     address public constant admin = 0x1111111111111111111111111111111111111111;
 
     modifier AssetsBridgeRequire() {
         require(_msgSender() == admin, "AssetsBridge: require called by the assets bridge admin address");
+
+        _;
+    }
+}
+
+abstract contract AssetsBridgeAdminOrOwner is Context, Ownable {
+    address public constant admin = 0x1111111111111111111111111111111111111111;
+
+    modifier AssetsBridgeRequire() {
+        require(_msgSender() == owner() || _msgSender() == admin, "AssetsBridge: require called by owner or admin");
 
         _;
     }
