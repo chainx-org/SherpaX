62433ed90632c2cc2b32db17e698981b1a8d479f
NingBo Wang <2536935847@qq.com>
2022-01-08 17:03:23 +0800

    add erc20 deployment script

---
+++ contracts/erc20-contracts/contracts/AssetsBridgeAdaptor.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/utils/Context.sol';
+/**
+ * @dev Interface of the SherpaX AssetsBridge
+ */
+interface IAssetsBridge {
+    /*
+     * @dev mint the token to account for assets bridge admin.
+     * @param account The receiver of token.
+     * @param amount The amount of token.
+     */
+    function mint_into(address account, uint256 amount) external returns (bool);
+
+    /*
+     * @dev burn the token from account for assets bridge admin.
+     * @param account The owner of token.
+     * @param amount The amount of token.
+     */
+    function burn_from(address account, uint256 amount) external returns (bool);
+}
+
+abstract contract AssetsBridgeAdmin is Context {
+    address public constant admin = 0x1111111111111111111111111111111111111111;
+
+    modifier AssetsBridgeRequire() {
+        require(_msgSender() == admin, "AssetsBridge: require called by the assets bridge admin address");
+
+        _;
+    }
+}
+
+abstract contract AssetsBridgeAdminOrOwner is Context, Ownable {
+    address public constant admin = 0x1111111111111111111111111111111111111111;
+
+    modifier AssetsBridgeRequire() {
+        require(_msgSender() == owner() || _msgSender() == admin, "AssetsBridge: require called by owner or admin");
+
+        _;
+    }
+}
---
+++ contracts/erc20-contracts/hardhat.config.js
@@ -0,0 +1,48 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('@nomiclabs/hardhat-ethers');
+
+// Change private keys accordingly - ONLY FOR DEMOSTRATION PURPOSES - PLEASE STORE PRIVATE KEYS IN A SAFE PLACE
+// Export your private key as
+//       export PRIVKEY=0x.....
+const privateKey = process.env.PRIVKEY;
+const privateKeyDev =
+   '0x99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342';
+
+module.exports = {
+   defaultNetwork: 'hardhat',
+
+   networks: {
+      hardhat: {},
+
+      dev: {
+         url: 'http://8.136.153.42:8546',
+         accounts: [privateKeyDev],
+         network_id: '1506',
+         chainId: 1506,
+      },
+   },
+   solidity: {
+      compilers: [
+         {
+            version: '0.8.0',
+            settings: {
+               optimizer: {
+                  enabled: true,
+                  runs: 200,
+               },
+            },
+         },
+      ]
+   },
+   paths: {
+      sources: './contracts',
+      cache: './cache',
+      artifacts: './artifacts',
+   },
+   mocha: {
+      timeout: 20000,
+   },
+};
