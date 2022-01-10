b81b8113a39ba6cdba46486a2221364adea4d391
icodezjb <icodezjb@users.noreply.github.com>
2022-01-10 20:27:05 +0800

    Transfer slash balance to Treasury

--- runtime/sherpax/src/lib.rs
+++ runtime/sherpax/src/lib.rs
@@ -445,8 +445,8 @@ impl pallet_elections_phragmen::Config for Runtime {
     type CandidacyBond = CandidacyBond;
     type VotingBondBase = VotingBondBase;
     type VotingBondFactor = VotingBondFactor;
-    type LoserCandidate = ();
-    type KickedMember = ();
+    type LoserCandidate = Treasury;
+    type KickedMember = Treasury;
     type DesiredMembers = DesiredMembers;
     type DesiredRunnersUp = DesiredRunnersUp;
     type TermDuration = TermDuration;
