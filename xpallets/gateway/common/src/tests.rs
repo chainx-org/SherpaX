9d08963da80c579a1d4f19638c75e90557030710
hacpy <hacpy.lbx@gmail.com>
2022-03-18 17:56:54 +0800

    Add test

--- xpallets/gateway/common/src/tests.rs
+++ xpallets/gateway/common/src/tests.rs
@@ -1,9 +1,10 @@
 // Copyright 2019-2020 ChainX Project Authors. Licensed under GPL-3.0.
 
 use crate::{
-    mock::{ExtBuilder, Test},
+    mock::{bob, charlie, ExtBuilder, Test, XGatewayCommon},
     Pallet, TrusteeSessionInfoLen,
 };
+use frame_support::assert_ok;
 use xp_assets_registrar::Chain;
 
 #[test]
@@ -15,4 +16,35 @@ fn test_do_trustee_election() {
 
         assert_eq!(TrusteeSessionInfoLen::<Test>::get(Chain::Bitcoin), 1);
     })
+}
+
+#[test]
+fn test_claim_not_native_asset_reward() {
+    ExtBuilder::default().build().execute_with(|| {
+        assert_ok!(XGatewayCommon::do_trustee_election());
+
+        TrusteeSigRecord::<Test>::mutate(Chain::Bitcoin, bob(), |record| *record = 9);
+        TrusteeSigRecord::<Test>::mutate(Chain::Bitcoin, charlie(), |record| *record = 1);
+
+        assert_eq!(XGatewayCommon::trustee_sig_record(bob()), 9);
+        assert_eq!(XGatewayCommon::trustee_sig_record(charlie()), 1);
+        assert_eq!(XGatewayCommon::trustee_sig_record(dave()), 0);
+
+        let multi_address = XGatewayCommon::trustee_multisig_addr(Chain::Bitcoin);
+
+        assert_ok!(XGatewayRecords::deposit(&multi_address, X_BTC, 10));
+
+        TrusteeSessionInfoOf::<Test>::mutate(Chain::Bitcoin, 1, |info| {
+            if let Some(info) = info {
+                info.0.trustee_list.iter_mut().for_each(|trustee| {
+                    trustee.1 = XGatewayCommon::trustee_sig_record(Chain::Bitcoin, &trustee.0);
+                });
+            }
+        });
+
+        assert_ok!(XGatewayCommon::apply_claim_trustee_reward(1));
+
+        assert_eq!(XAssets::usable_balance(&bob(), &X_BTC), 9);
+        assert_eq!(XAssets::usable_balance(&charlie(), &X_BTC), 1);
+    });
 }
